575f5d226e17d03cb7b392890e326a4d86762b69
Ikey Benzaken <ikey.benz@gmail.com>
2015-06-29 15:21:43 -0400

    Completed Make School Peeved Penguins Game

---
+++ PeevedPenguins.spritebuilder/Seal.swift
@@ -0,0 +1,13 @@
+//
+//  Sesl.swift
+//  PeevedPenguins
+//
+//  Created by Ikey Benzaken on 6/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Seal: CCSprite {
+    
+}
